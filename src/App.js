21c07bc266882f781989faa6b60de81433d30540
mouraeduardo <eduardomoura2001@hotmail.com>
2023-09-17 20:55:31 -0300

    commit all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [dados, setDados] = useState([]);
+
+  const EnviarDados = (e) => {
+    e.preventDefault();
+    const novoDado = {
+      nome,
+      sobrenome,
+    };
+    setDados([...dados, novoDado]);
+    setNome('');
+    setSobrenome('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Formulário de Nome e Sobrenome</h1>
+      <form onSubmit={EnviarDados}>
+        <label>
+          Nome:
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+        </label>
+        <label>
+          Sobrenome:
+          <input
+            type="text"
+            value={sobrenome}
+            onChange={(e) => setSobrenome(e.target.value)}
+          />
+        </label>
+        <button type="submit">Adicionar</button>
+      </form>
+
+      <h2>Tabela de Dados:</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Sobrenome</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dados.map((item, index) => (
+            <tr key={index}>
+              <td>{item.nome}</td>
+              <td>{item.sobrenome}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
